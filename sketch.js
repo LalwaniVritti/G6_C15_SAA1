b42ecc2b361183eecf6ce4addab9cba3cf666fee
LalwaniVritti <88480121+LalwaniVritti@users.noreply.github.com>
2021-08-16 15:55:01 +0530

    Add files via upload
    Adding Money in Piggy Bank. Class 15 Additional Activity

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 var piggyBank,piggyBankImg;
-var coin,coinImg;
+var coin,coinImg,coin2Img;
+const PLAY = 1;
+const END =0;
+var gameState=PLAY;
 
 function preload(){
   piggyBankImg=loadImage("pbank.png");
-  coinImg=loadImage("200.jpeg")
+  coinImg=loadImage("coin.png");
+  coin2Img = loadImage("coin2.png");
 }
 function setup() {
   createCanvas(400, 400);
@@ -22,9 +26,20 @@ function setup() {
 function draw() {
   background("lightblue");
 
- if(mousePressedOver(coin)){
-   coin.velocityY = 3
- }
+  if(gameState === PLAY){
+    if(mousePressedOver(coin)){
+      coin.addImage(coin2Img);
+      coin.scale=0.03;
+      coin.velocityY = 5;
+      gameState = END;
+    }
+  }
+
+  else if(gameState === END ){
+    textSize(20);
+    fill("red");
+    text("$$ You added one dollar $$",100,50);
+  }
  
   drawSprites();
   
